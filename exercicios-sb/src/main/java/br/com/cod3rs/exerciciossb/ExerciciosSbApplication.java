f91e9d5a666946407d9487137e10a33db8a71f36
Rodrigo Barros <rodrigosbarros51@gmail.com>
2022-04-28 23:56:52 -0300

    commit inicial

---
+++ exercicios-sb/src/main/java/br/com/cod3rs/exerciciossb/ExerciciosSbApplication.java
@@ -0,0 +1,13 @@
+package br.com.cod3rs.exerciciossb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosSbApplication.class, args);
+	}
+
+}
---
+++ exercicios-sb/src/main/java/br/com/cod3rs/exerciciossb/controllers/PrimeiroController.java
@@ -0,0 +1,18 @@
+package br.com.cod3rs.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+
+    @GetMapping(path = {"/ola", "/saudacao"})
+    public String olar(){
+        return "Olá Spring Boot!";
+    }
+
+
+
+}
